ed234d3ec37f4dae52497562542c7a5d60af1c3c
alelouet <antoinelelouet@msn.com>
2025-02-18 23:08:59 +0100

    Clean footer of copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,6 @@ export const Footer = () => {
             className="font-bold text-xl flex"
           >
             <LogoIcon />
-            ShadcnUI/React
           </a>
         </div>
 
@@ -152,14 +151,14 @@ export const Footer = () => {
 
       <section className="container pb-14 text-center">
         <h3>
-          &copy; 2024 Landing page made by{" "}
+          &copy; 2025 Planifestival made by{" "}
           <a
             rel="noreferrer noopener"
             target="_blank"
-            href="https://www.linkedin.com/in/leopoldo-miranda/"
+            href="https://www.linkedin.com/in/antoine-le-lou%C3%ABt/"
             className="text-primary transition-all border-primary hover:border-b-2"
           >
-            Leo Miranda
+            Antoine LE LOUËT
           </a>
         </h3>
       </section>
