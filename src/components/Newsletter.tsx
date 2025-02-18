c5f8fbd21f341432d0bbc91218f9f8164db5647d
alelouet <antoinelelouet@msn.com>
2025-02-18 23:25:50 +0100

    Update Newsletter module

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -5,6 +5,7 @@ export const Newsletter = () => {
   const handleSubmit = (e: any) => {
     e.preventDefault();
     console.log("Subscribed!");
+    // TODO : Faire le fonctionnel pour enregistré les emails.
   };
 
   return (
@@ -13,25 +14,28 @@ export const Newsletter = () => {
 
       <div className="container py-24 sm:py-32">
         <h3 className="text-center text-4xl md:text-5xl font-bold">
-          Join Our Daily{" "}
+          Rejoignez{" "}
           <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-            Newsletter
+            PlaniFestival
           </span>
+          {" "}en avant-première
         </h3>
         <p className="text-xl text-muted-foreground text-center mt-4 mb-8">
-          Lorem ipsum dolor sit amet consectetur.
+          Inscrivez-vous dès maintenant pour accéder en exclusivité à notre plateforme et ne rien manquer des futurs lancements.
         </p>
 
         <form
           className="flex flex-col w-full md:flex-row md:w-6/12 lg:w-4/12 mx-auto gap-4 md:gap-2"
           onSubmit={handleSubmit}
+          // TODO : faire de la validation d'email
+          // TODO : ajouter captcha
         >
           <Input
-            placeholder="leomirandadev@gmail.com"
+            placeholder="Votre adresse e-mail"
             className="bg-muted/50 dark:bg-muted/80 "
             aria-label="email"
           />
-          <Button>Subscribe</Button>
+          <Button>Je m’inscris</Button>
         </form>
       </div>
 
